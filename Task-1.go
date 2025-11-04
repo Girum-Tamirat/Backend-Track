82bc9aa39f0b149ed2f44ffcca7151117e766482
Girum-Tamirat <girumtamirat10@gmail.com>
2025-11-04 21:25:07 +0300

    task 1 done

--- Task-1.go
+++ Task-1.go
@@ -1,8 +1,34 @@
+// Fundamentals of Go Tasks
+// Task: Sum of Numbers
+// Write a Go function that takes a slice of integers as input and returns the sum of all the numbers. If the slice is empty, the function should return 0.
+// [Optional]: Write a test for your function
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func hello(){
-	fmt.Println("Hello world")
+func sum_of_numbers(nums[] int) int{
+	if len(nums) == 0 {
+		return 0
+	}
+	s := 0
+	for _, num := range nums {
+		s += num
+	}
+	return s
 }
 
+func main() {
+	var n, num int
+	fmt.Print("How many numbers? ")
+	fmt.Scan(&n)
+	nums := make([]int, n)
+	for i:=0; i<n; i++ {
+		fmt.Printf("Enter number %d", i+1)
+		fmt.Scan(&num)
+		nums = append(nums, num)
+	}
+	fmt.Println(sum_of_numbers(nums))
+}
